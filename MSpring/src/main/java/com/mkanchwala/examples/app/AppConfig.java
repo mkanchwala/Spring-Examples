0fbf5aed84bfbce8455ba052b5f7bdbd5d66172b
mkanchwala <murtuza.rajkahn@gmail.com>
2015-04-17 17:19:10 +0530

    Initial Commit

---
+++ MSpring/src/main/java/com/mkanchwala/examples/app/AppConfig.java
@@ -0,0 +1,10 @@
+package com.mkanchwala.examples.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.mkanchwala.examples")
+public class AppConfig {
+
+}
